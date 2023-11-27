dc370315a4a86b9afdee5e55d1542fd0ad2f12a4
Gonzalo Valenzuela <gonzavalenzuela23@gmail.com>
2023-11-27 20:13:46 -0300

    agregando el esquema completo de la tabla de predicciones

--- app/schema.py
+++ app/schema.py
@@ -27,12 +27,44 @@ instructions = [
             id_user INT NOT NULL,
             symbol VARCHAR(6) NOT NULL,
             date_created TIMESTAMP NOT NULL,
-            date_prediction TIMESTAMP NOT NULL,
-            price_created FLOAT NOT NULL,
-            price_prediction FLOAT NOT NULL,
+            price_created FLOAT NOT NULL, 
+            
+            price_pred_open_30_1 FLOAT NOT NULL,
+            price_pred_close_30_1 FLOAT NOT NULL,
+            price_pred_max_30_1 FLOAT NOT NULL,
+            price_pred_min_30_1 FLOAT NOT NULL,
+
+            price_pred_open_30_3 FLOAT NOT NULL,
+            price_pred_close_30_3 FLOAT NOT NULL,
+            price_pred_max_30_3 FLOAT NOT NULL,
+            price_pred_min_30_3 FLOAT NOT NULL,
+
+
+            price_pred_open_30_7 FLOAT NOT NULL,
+            price_pred_close_30_7 FLOAT NOT NULL,
+            price_pred_max_30_7 FLOAT NOT NULL,
+            price_pred_min_30_7 FLOAT NOT NULL,
+
+
+            price_pred_open_07_1 FLOAT NOT NULL,
+            price_pred_close_07_1 FLOAT NOT NULL,
+            price_pred_max_07_1 FLOAT NOT NULL,
+            price_pred_min_07_1 FLOAT NOT NULL,
+
+            price_pred_open_07_3 FLOAT NOT NULL,
+            price_pred_close_07_3 FLOAT NOT NULL,
+            price_pred_max_07_3 FLOAT NOT NULL,
+            price_pred_min_07_3 FLOAT NOT NULL,
+
+
+            price_pred_open_07_7 FLOAT NOT NULL,
+            price_pred_close_07_7 FLOAT NOT NULL,
+            price_pred_max_07_7 FLOAT NOT NULL,
+            price_pred_min_07_7 FLOAT NOT NULL,
+
             FOREIGN KEY (id_user) REFERENCES users(id),
             FOREIGN KEY (symbol) REFERENCES stocks(symbol)
-        );
+        )
     """,
     """
         INSERT INTO stocks (symbol, name) VALUES
@@ -51,6 +83,5 @@ instructions = [
         INSERT INTO users (username, email, password, nombre, apellido) VALUES
             ('user', 'user@gmail.com', 'scrypt:32768:8:1$dlRVcZNvwWPLgAO2$d7a47c0d6787b0d55b4ad375b31e4e63e7f903403ce19d3ab668de8ab75fc32478901645c73e70c27f0a642060389972c1b183dabe8f1a5bbe6e9d8621a94eab', 'user', 'user')
     """
-
 ]
 
