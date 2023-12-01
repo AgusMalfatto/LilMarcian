0cf4d23de927955f48ee8da223096e2f4a5f43a7
GatitoEnojado <ibottaro99@gmail.com>
2023-12-01 01:34:53 -0300

    Agregando comentarios

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,9 +1,11 @@
+# Importa functools para funciones de orden superior y elementos necesarios de Flask
 import functools
 from flask import(
     Blueprint, flash, g, redirect, render_template, request, url_for, session, redirect
 )
 
-# Creacion de Blueprint
+# Crea un Blueprint llamado 'auth' con prefijo de URL '/auth'
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+# Importa las vistas asociadas al Blueprint desde el módulo actual (views.py)
 from . import views
